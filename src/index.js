69b148db243329167001eca23e5031e154b7d0dc
developervilchez <developervilchez@gmail.com>
2018-03-12 12:56:17 -0500

    transform a class component to functional components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
 import App from './components/app';
-import reducers from './reducers';
+import reducer from './reducers/index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+render(
+  <Provider store={store}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.getElementById('root')
+)
