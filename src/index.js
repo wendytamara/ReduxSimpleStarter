877de6b8718b89cd2173e9e67f5d35807c817381
developervilchez <developervilchez@gmail.com>
2018-03-12 15:14:35 -0500

    files base are ready!

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import App from './components/app';
-import reducer from './reducers/index';
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 )
