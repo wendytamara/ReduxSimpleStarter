e32c930f86b4641250a1d77a5246714f43dfbe7f
developervilchez <developervilchez@gmail.com>
2018-03-12 12:59:55 -0500

    change the component app class to functional

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
-}
+const App = () => (
+  <div>Started with react and redux</div>
+)
+
+export default App
